43304f861d7660913b4c287c0ca6c56d410b91cc
unknown <kiril.rusev2000@gmail.com>
2020-04-05 17:00:11 +0300

    Fix functions and parameters names and add constants in Game and TurnHandler

--- src/chess/playboard/Game.java
+++ src/chess/playboard/Game.java
@@ -2,77 +2,60 @@ package chess.playboard;
 
 import chess.pieces.*;
 
-/**
- * The ChessGame class is used for holding chess objects
- * which contains the board, pieces and handles alternating turns.
- */
 public class Game {
 
-    private TurnHandler chessBoard;
-    private King player1King;
-    private King player2King;
+    private static final int STARTING_SIDE_OF_PLAYER1 = 0;
+    private static final int STARTING_SIDE_OF_PLAYER2 = 7;
+    private static final int BOARD_SIZE = 8;
 
-    /**
-     * Create new instances of necessary properties.
-     */
+    private TurnHandler turnHandler;
+    private King kingOfPlayer1;
+    private King kingOfPlayer2;
+
     public Game() {
-        chessBoard = new TurnHandler();
-        setupTeam(0, "player1");
-        setupTeam(7, "player2");
+        turnHandler = new TurnHandler();
+        setupTeam(STARTING_SIDE_OF_PLAYER1, "player1");
+        setupTeam(STARTING_SIDE_OF_PLAYER2, "player2");
     }
 
-    /**
-     * Sets up pieces for each player.
-     * @param side Starting side of the player
-     * @param player String of the player
-     */
     private void setupTeam(int side, String player) {
-        int one = (side > 0) ? -1: 1;
-        int colIncerment = 0;
+        int direction = (side > 0) ? -1 : 1;
+        int currentColumn = 0;
 
-        // Rook
-        Piece r1 = new Rook(player, new Location(side, colIncerment), this);
-        Piece r2 = new Rook(player, new Location(side, 7-colIncerment), this);
-        colIncerment += 1;
+        Piece r1 = new Rook(player, new Location(side, currentColumn), this);
+        Piece r2 = new Rook(player, new Location(side, BOARD_SIZE - 1 - currentColumn), this);
+        currentColumn += 1;
 
-        // Knight
-        Piece n1 = new Knight(player, new Location(side, colIncerment), this);
-        Piece n2 = new Knight(player, new Location(side, 7-colIncerment), this);
-        colIncerment += 1;
+        Piece n1 = new Knight(player, new Location(side, currentColumn), this);
+        Piece n2 = new Knight(player, new Location(side, BOARD_SIZE - 1 - currentColumn), this);
+        currentColumn += 1;
 
-        // Bishop
-        Piece b1 = new Bishop(player, new Location(side, colIncerment), this);
-        Piece b2 = new Bishop(player, new Location(side, 7-colIncerment), this);
-        colIncerment += 1;
+        Piece b1 = new Bishop(player, new Location(side, currentColumn), this);
+        Piece b2 = new Bishop(player, new Location(side, BOARD_SIZE - 1 - currentColumn), this);
+        currentColumn += 1;
 
-        // King & Queen
         if (player.equalsIgnoreCase("player1")) {
-            player1King = new King(player, new Location(side, colIncerment), this);
+            kingOfPlayer1 = new King(player, new Location(side, currentColumn), this);
         } else {
-            player2King = new King(player, new Location(side, colIncerment), this);
+            kingOfPlayer2 = new King(player, new Location(side, currentColumn), this);
         }
 
-        Piece q = new Queen(player, new Location(side, 7-colIncerment), this);
+        Piece q = new Queen(player, new Location(side, BOARD_SIZE - 1 - currentColumn), this);
 
-        // Pawns
-        for (int i = 0; i < 8; i++) {
-            Piece p = new Pawn(player, new Location(side + one, i), this);
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            Piece p = new Pawn(player, new Location(side + direction, i), this);
         }
     }
 
-    /**
-     * Returns the ChessBoard.
-     * @return The board object of the chess game.
-     */
-    public TurnHandler getChessBoard() {
-        return chessBoard;
+    public TurnHandler getTurnHandler() {
+        return turnHandler;
     }
 
-    public King getPlayer1King() {
-        return player1King;
+    public King getKingOfPlayer1() {
+        return kingOfPlayer1;
     }
 
-    public King getPlayer2King() {
-        return player2King;
+    public King getKingOfPlayer2() {
+        return kingOfPlayer2;
     }
 }
--- src/chess/playboard/TurnHandler.java
+++ src/chess/playboard/TurnHandler.java
@@ -3,92 +3,52 @@ package chess.playboard;
 import chess.pieces.Piece;
 import chess.pieces.Location;
 
-/**
- * The Chessboard class is responsible for manipulating the board
- * by moving and removing pieces.
- */
 public class TurnHandler {
+
+    private static final int BOARD_SIZE = 8;
     private Piece[][] board;
 
-    /**
-     * Creates the board instance.
-     */
     public TurnHandler() {
-        board = new Piece[8][8];
+        board = new Piece[BOARD_SIZE][BOARD_SIZE];
     }
 
-    /**
-     * Checks if piece is at a specificed location.
-     * @param row Row of the location.
-     * @param col Col of the location.
-     * @return Boolean of if theres a piece on location or not.
-     */
     public boolean isPieceAt(int row, int col) {
         return board[row][col] != null;
     }
 
-    /**
-     * Places piece at location, if piece already exisit there, the old piece will be overwritten.
-     * @param piece The piece to move.
-     * @param location The location to move to.
-     */
     public void placePieceAt(Piece piece, Location location) {
         if (isPieceAt(location.getRow(), location.getCol())) {
-            removePieceAt(location);
+            removePieceFrom(location);
         }
         if (piece.getLocation() != null) {
-            removePieceAt(piece.getLocation());
+            removePieceFrom(piece.getLocation());
         }
         board[location.getRow()][location.getCol()] = piece;
         piece.setLocation(location);
     }
 
-    /**
-     * Removes piece at location.
-     * @param location The location to remove at.
-     */
-    private void removePieceAt(Location location) {
+    private void removePieceFrom(Location location) {
         board[location.getRow()][location.getCol()] = null;
     }
 
-    /**
-     * Checks if the location is within the board.
-     * @param location The location to check.
-     * @return Boolean of if the location is in bounds or not.
-     */
-    public static boolean locationInBounds(Location location) {
-        return location.getRow() >= 0 && 
-               location.getRow() < 8 && 
-               location.getCol() >= 0 &&
-               location.getCol() < 8;
-    }
-
-    /**
-     * Gets piece at location of board.
-     * @param location Location to find piece at.
-     * @return Piece at location/
-     */
     public Piece getPieceAt(Location location) {
         return board[location.getRow()][location.getCol()];
     }
 
-    /**
-     * Displays board with P being Piece.
-     */
     @Override
     public String toString() {
-        String s = "  0 1 2 3 4 5 6 7\n";
-        for (int row = 0; row < 8; row++) {
-            s += row;
-            for (int col = 0; col < 8; col++) {
+        StringBuilder s = new StringBuilder("  0 1 2 3 4 5 6 7\n");
+        for (int row = 0; row < BOARD_SIZE; row++) {
+            s.append(row);
+            for (int col = 0; col < BOARD_SIZE; col++) {
                 if (board[row][col] != null) {
-                    s += " " + board[row][col].getType();
+                    s.append(" ").append(board[row][col].getType());
                 } else {
-                    s += " -";
+                    s.append(" -");
                 }
             }
-            s += "\n";
+            s.append("\n");
         }
-        return s;
+        return s.toString();
     }
 }
